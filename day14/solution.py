c55f1f2a777168edc1952f6b461d8cb5cb87c821
adrian kocifaj <adrian.kocifaj@gmail.com>
2024-10-22 08:49:54 +0200

    day14 2nd part solution

--- day14/solution.py
+++ day14/solution.py
@@ -14,15 +14,48 @@ def read_file() -> None:
             memory_instructions[-1].append((address, value))
 
 
+def set_bit_to_zero(number: int, bit_index: int) -> int:
+    return number & ~(1 << bit_index)
+
+
+def set_bit_to_one(number: int, bit_index: int) -> int:
+    return number | (1 << bit_index)
+
+
 def calculate_value(mask: str, value: int) -> int:
     for i in range(len(mask)):
         bit = mask[len(mask) - i - 1]
         if bit == "0":
-            value = value & ~(1 << i)
+            value = set_bit_to_zero(value, i)
         elif bit == "1":
-            value = value | (1 << i)
+            value = set_bit_to_one(value, i)
 
     return value
+
+
+def calculate_addresses(mask: str, address: int) -> set[int]:
+    addresses = set()
+    indexes = []
+    for i in range(len(mask)):
+        bit = mask[len(mask) - i - 1]
+        if bit == "1":
+            address = set_bit_to_one(address, i)
+        elif bit == "X":
+            indexes.append(i)
+
+    for i in range(2 ** len(indexes)):
+        storage = address
+        for j in range(len(indexes)):
+            bit = 1 & (i >> j)
+            bit_index = indexes[j]
+            if bit == 1:
+                storage = set_bit_to_one(storage, bit_index)
+            else:
+                storage = set_bit_to_zero(storage, bit_index)
+            storage = storage | (bit << bit_index)
+        addresses.add(storage)
+
+    return addresses
 
 
 def star1() -> int:
@@ -36,7 +69,15 @@ def star1() -> int:
 
 
 def star2() -> int:
-    return 0
+    for i in range(len(masks)):
+        mask = masks[i]
+        for mi in memory_instructions[i]:
+            address, value = mi
+            addresses = calculate_addresses(mask, address)
+            for add in addresses:
+                memory[add] = value
+
+    return sum(memory.values())
 
 
 if __name__ == "__main__":
@@ -47,4 +88,5 @@ if __name__ == "__main__":
     read_file()
 
     print(star1())
+    memory.clear()
     print(star2())
